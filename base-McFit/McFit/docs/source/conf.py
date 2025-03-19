b13373fde2def839a8e8855c8f425b1f582f60ce
AndresCook <88800334+AndresCook@users.noreply.github.com>
2025-03-19 14:33:57 -0500

    Update doc theme

--- base-McFit/McFit/docs/source/conf.py
+++ base-McFit/McFit/docs/source/conf.py
@@ -20,7 +20,8 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.autosummary']
+              'sphinx.ext.autosummary',
+              'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -36,5 +37,5 @@ def setup(app):
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
